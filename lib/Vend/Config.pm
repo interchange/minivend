163c55ca81d33d0297794816632f6600a8a1b698
Mike Heins <mike@perusion.com>
2000-12-30 15:17:11 +0000

    Fix problem with ProductFiles resolution in loops. The Vend::OnlyProducts setting was confused after the on-the-fly update.
    Removed original setting code in interchange.PL and pre-set code in
    Config.pm, placed optimization setting in Vend::Data::update_productbase
    where it belogs.

--- lib/Vend/Config.pm
+++ lib/Vend/Config.pm
@@ -1650,14 +1650,6 @@ my %Default = (
 							for(@$setting) {
 								next unless defined $C->{Database}{$_}{'file'};
 								push @out, $C->{Database}{$_}{'file'};
-								if( defined $Vend::Cfg->{OnlyProducts} ) {
-									undef $Vend::Cfg->{OnlyProducts};
-									next;
-								}
-								else {
-									$Vend::Cfg->{OnlyProducts} = 
-										$C->{Database}{$_}{'name'};
-								}
 							}
 							unless (scalar @out) {
 								return 1 if $C->{BaseCatalog};
