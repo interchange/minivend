f850b498428233f5a1568e7d9c55eb097c21ba5b
Kevin Walsh <kevin@cursor.biz>
2002-01-31 07:27:42 +0000

    Tag code files with an unknown file extension will now be silently ignored instead of being assumed to be UserTag files.
    This allows for backup/old files to be kept in the same directory,
    as is normally expected.

--- lib/Vend/Config.pm
+++ lib/Vend/Config.pm
@@ -987,9 +987,9 @@ sub get_system_code {
 	for(@files) {
 		next if m{^\.};
 		next if m{/\.};
-		next unless m{\.(\w+)$};
-		my $ext = $1;
-		$CodeDest = $extmap{lc $ext} || 'UserTag';
+		next unless m{/[^.]+\.(\w+)$};
+
+		$CodeDest = $extmap{lc $1} or next;
 		open SYSTAG, "< $_"
 			or config_error("read system tag file %s: %s", $_, $!);
 		while(<SYSTAG>) {
