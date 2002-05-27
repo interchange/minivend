1f9812701da3a0033549cc2df7377718f8f07f50
Stefan Hornburg <racke@linuxia.de>
2002-05-27 12:06:11 +0000

    storage of MAP attribute revised

--- lib/Vend/Config.pm
+++ lib/Vend/Config.pm
@@ -2789,6 +2789,21 @@ sub parse_database {
 				$c->{$val} = $d;
 			}
 		}
+		elsif ($p eq 'MAP') {
+			my @f = split(/\s+/, $val);
+			if (@f < 2) {
+				config_error("At least two parameters needed for MAP.");
+			} elsif (@f > 3) {
+				config_error("Too much parameters for MAP.");
+			} elsif (@f == 2) {
+				@f = ($f[0], 'default', $f[1]);
+			}
+			$d->{OrigClass} = $d->{Class};
+			$d->{Class} = 'SHADOW';
+			$d->{type} = 10;
+			$d->{MAP}->{$f[0]}->{$f[1]} = $f[2];
+		}
+
 		else {
 			defined $d->{$p}
 				and
@@ -2799,11 +2814,6 @@ sub parse_database {
 					$d->{$p},
 				);
 			$d->{$p} = $val;
-		}
-		if ($p eq 'MAP') {
-			$d->{OrigClass} = $d->{Class};
-			$d->{Class} = 'SHADOW';
-			$d->{type} = 10;
 		}
 		$d->{HOT} = 1 if $d->{Class} eq 'MEMORY';
 	}
