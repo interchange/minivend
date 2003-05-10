01a99c136ec3bf029d607c7a28461cb5f1fc41f1
Mike Heins <mike@perusion.com>
2003-05-10 22:06:14 +0000

    * Don't add .html if file already has extension...

--- lib/Vend/Menu.pm
+++ lib/Vend/Menu.pm
@@ -1254,7 +1254,12 @@ sub tree_link {
 		unless($row->{form} =~ /[\r\n]/) {
 			$row->{form} = join "\n", split $Global::UrlSplittor, $row->{form};
 		}
-		$row->{href} = Vend::Tags->area( { href => $row->{page}, form => $row->{form} });
+		my $add = ($::Scratch->{mv_add_dot_html} && $row->{page} !~ /\.\w+$/) || 0;
+		$row->{href} = Vend::Tags->area({
+							href => $row->{page},
+							form => $row->{form},
+							add_dot_html => $add,
+						});
 	}
 	$row->{name} =~ s/ /&nbsp;/g;
 	$opt->{toggle_base_url} ||= Vend::Tags->history_scan(
@@ -1353,10 +1358,13 @@ sub tree_line {
 			$form = join "\n", split $Global::UrlSplittor, $form;
 		}
 
+		my $add = ($::Scratch->{mv_add_dot_html} && $row->{page} !~ /\.\w+$/) || 0;
+
 		$row->{page} = Vend::Tags->area({
 								href => $row->{page},
 								form => $form,
 								no_count => $opt->{timed},
+								add_dot_html => $add,
 								no_session_id => $opt->{timed}
 							});
 
@@ -1436,7 +1444,14 @@ EOF
 		unless($row->{form} =~ /[\r\n]/) {
 			$row->{form} = join "\n", split $Global::UrlSplittor, $row->{form};
 		}
-		$row->{href} = Vend::Tags->area( { href => $row->{page}, form => $row->{form} });
+		my $add = $::Scratch->{mv_add_dot_html} && $row->{page} !~ /\.\w+$/;
+
+		$row->{href} = Vend::Tags->area(
+								{
+									href => $row->{page},
+									form => $row->{form},
+									add_dot_html => $add,
+								});
 	}
 	extra_value($opt->{extra_value}, $row)
 			if $opt->{extra_value};
